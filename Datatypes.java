29a82ce5bbcdc19c6d8de0695274aee95fabc9c8
sriram <sriramoholic254@gmail.com>
2024-03-01 20:44:12 +0530

    Added a new file Datatypes.java and practiced all kinds of Primitive data types

---
+++ Datatypes.java
@@ -0,0 +1,39 @@
+class Datatypes{
+    public static void main(String a[]){
+
+        Integer
+        int num = 5;
+        System.out.println(num);
+
+        long longNum = 456728334876587l;
+        System.out.println(longNum);
+
+        short shortNum = 456;
+        System.out.println(shortNum);
+
+        byte by = 1;
+        System.out.println(by);
+
+        Double
+        float floatNum = 5.8f;
+        System.out.println(floatNum);
+
+        double doubleNum = 5.88989;
+        System.out.println(doubleNum);
+
+        Char
+        char name = 'S';
+        System.out.println(name);
+
+        String user = "Sriram";
+        System.out.println(user);
+
+        Boolean
+        boolean bool = true;
+        System.out.println(bool);
+
+        boolean bool = false;
+        System.out.println(bool);
+        
+    }
+}
