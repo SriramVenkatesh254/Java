38b266b55461abf7ae242d4c266370fd79fdcbd5
sriram <sriramoholic254@gmail.com>
2024-03-02 13:21:06 +0530

    Created a file TypeComvertion.java

---
+++ TypeConvertion.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class typeConvertion{
+    public static void main(String a[]){
+        byte physics = 100;
+        byte chemistry = 100;
+        int result = physics + chemistry;
+        System.out.println(result);
+        System.out.println(((Object)result).getClass().getSimpleName());
+    }
+}
