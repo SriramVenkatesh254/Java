e9d1e6857cf960a14a2f7158a99dbb7a2dc38384
sriram <sriramoholic254@gmail.com>
2024-03-02 13:04:26 +0530

    Added a new file TypeCasting.java

---
+++ TypeCasting.java
@@ -0,0 +1,14 @@
+class typeCasting {
+    public static void main(String a[]){
+
+        //Int to Byte
+        int num = 50;
+        byte by = (byte) num;
+        System.out.println(by);
+
+       //Double to int 
+        double grade = 9.8;
+        int intGrade = (int) grade;
+        System.out.println(intGrade);
+    }
+}
