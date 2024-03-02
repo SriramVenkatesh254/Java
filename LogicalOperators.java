218ebbb5ad5f821411513ce9acd65f764fe07a9d
sriram <sriramoholic254@gmail.com>
2024-03-02 19:58:59 +0530

    Created a file LogicalOperators.java and did some operations related to it

---
+++ LogicalOperators.java
@@ -0,0 +1,32 @@
+class LogicalOperators{
+    public static void main(String an[]){
+
+        //Logical Operators
+
+        // && -> Logical AND
+        // || -> Logical OR
+        // ! -> Logical NOT
+
+        //Logical AND &&
+        int x = 8;
+        int y = 5;
+        int a = 1;
+        int b = 7;
+        boolean result = x > y && a < b;
+        System.out.println(result); //true
+
+        // Logical OR ||
+        int x = 15;
+        int y = 13;
+        int a = 7;
+        int b = 90;
+        boolean result = x < y || a > b;
+        System.out.println(result); //false
+
+        //Logical NOT 
+        int x = 10;
+        int y = 20;
+        boolean result = x > y;
+        System.out.println(!result); //true
+    }
+}
