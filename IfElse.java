0f8f7856d516f2b26d205b659a2ebc960f854fa7
sriram <sriramoholic254@gmail.com>
2024-03-02 21:18:41 +0530

    Created a file, IfElse.java and a small demo program on it

---
+++ IfElse.java
@@ -0,0 +1,17 @@
+class IfElse{
+    public static void main(String a[]){
+        int x = 1;
+        int y = 2;
+
+        if( x > y){
+            //This block will get executed if the condition is true.
+            System.out.println(x);
+            System.out.println("Thank You");
+        }
+        else{
+            //This block will get executed if the condition if false.
+            System.out.println(y);
+            System.out.println("Thank You");
+        }
+    }
+}
