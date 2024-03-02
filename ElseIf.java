2fcb2619835bbf73444a1a8d47c94e94a36fe44c
sriram <sriramoholic254@gmail.com>
2024-03-02 22:16:59 +0530

    Made some changes in ElseIf.java and created a new file, TernaryOperators.java and did a small demo progam in it

--- ElseIf.java
+++ ElseIf.java
@@ -1,13 +1,13 @@
 class ElseIf{
     public static void main(String[] args){
-        int x = 10;
-        int y = 20;
-        int z = 30;
+        int x = 60;
+        int y = 50;
+        int z = 70;
 
         if (x > y && x > z){
             System.out.println("X is greater than Y and Z");
         }
-        else if(y > x && y > z){
+        else if( y > z){
             System.out.println("Y is greater than X and Z");
         }
         else{
---
+++ TernaryOperators.java
@@ -0,0 +1,7 @@
+class TernaryOperators{
+    public static void main(String[] args){
+        int userInput = 10;
+        String result = (userInput % 2 == 0) ? "Even" : "Odd";
+        System.out.println(result);
+    }
+}
