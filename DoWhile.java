c56e2a4172b3d52363f8f5c7ed081500b115c3b8
sriram <sriramoholic254@gmail.com>
2024-03-04 23:06:01 +0530

    Modified WhileLoop and created a new file DoWhile and did some stuffs on it

---
+++ DoWhile.java
@@ -0,0 +1,9 @@
+class DoWhile{
+    public static void main(String a[]){
+        int i = 10;
+        do{
+            System.out.println("DoWhile block");
+            i++;
+        }while( i <= 4);
+    }
+}
