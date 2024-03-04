1b9e10732f4c6beec822272826c45e8010b713eb
sriram <sriramoholic254@gmail.com>
2024-03-04 20:12:55 +0530

    Modified a file, TernaryOperators.java

--- TernaryOperators.java
+++ TernaryOperators.java
@@ -1,5 +1,5 @@
 class TernaryOperators{
-    public static void main(String[] args){
+    public static void main(String args[]){
         int userInput = 10;
         String result = (userInput % 2 == 0) ? "Even" : "Odd";
         System.out.println(result);
