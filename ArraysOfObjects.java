7e3cd48db715219cf62996e030bf7e40cca5e50d
sriram <sriramoholic254@gmail.com>
2024-03-10 23:56:27 +0530

    Modified ArraysOfObjects.java

--- ArraysOfObjects.java
+++ ArraysOfObjects.java
@@ -16,15 +16,10 @@ public class ArraysOfObjects {
         s2.name = "Bala";
         s2.marks = 80;
 
-        Student s3 = new Student();
-        s3.rollNumber = 3;
-        s3.name = "Harees";
-        s3.marks = 99;
 
         Student students[] = new Student[3];
         students[0] = s1;
         students[1] = s2;
-        students[2] = s3;
 
         for(int i = 0; i < students.length; i++){
             System.out.println(students[i].name + " : " + students[i].marks);
