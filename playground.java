5691c64899711a340e38e26dfdceb379df576178
sriram <sriramoholic254@gmail.com>
2024-03-13 19:41:27 +0530

    .gitignore check

--- playground.java
+++ playground.java
@@ -1,3 +1,40 @@
-public class playground {
+class Cricket{
+    private String name;
+    private int intlRuns;
+    private String role;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getIntlRuns() {
+        return intlRuns;
+    }
+    public void setIntlRuns(int runs) {
+        intlRuns = runs;
+    }
+    public String getRole() {
+        return role;
+    }
+    public void setRole(String r) {
+        role = r;
+    }
+
     
+}
+
+public class playground{
+    public static void main(String args[]){
+        Cricket obj = new Cricket();
+        obj.setName("Virat Kohli");
+        System.out.println(obj.getName());
+
+        // obj.setIntlRuns(10_000);
+        // System.out.println(obj.getIntlRuns());
+
+        // obj.setRole("Batsman");
+        // System.out.println(obj.getRole());
+    }
 }
