17a941fb8891ee4acca2dab8be6f359add6c9348
sriram <sriramoholic254@gmail.com>
2024-03-16 23:42:50 +0530

    Added 2 new files, ThisMethod.java and SuperMethod()

--- SuperMethod.java
+++ SuperMethod.java
@@ -1,28 +1,31 @@
-//this() will execute the constructor of the same class.
+//super() will execute the constructor of the parent class or the superclass.
 
 class A{
     A(){
+        super();
         System.out.println("Inside class A's constructor");
     }
     A(int x){
+        super();
         System.out.println("Inside class A's param constructor"); //this
     }
 }
 
 class B extends A{
     B(){
+        super(5);
         System.out.println("Inside class B's constructor"); //this
     }
 
     B(int x){
-        this();
+        super();
         System.out.println("Inside class B's param constructor");
     }
 }
 
-public class playground{
+public class SuperMethod{
     public static void main(String args[]){
         // B obj = new B(5);
-        B obj = new B(5);
+        B obj = new B();
     }
 }
--- ThisMethod.java
+++ ThisMethod.java
@@ -20,7 +20,7 @@ class B extends A{
     }
 }
 
-public class playground{
+public class ThisMethod{
     public static void main(String args[]){
         // B obj = new B(5);
         B obj = new B(5);
