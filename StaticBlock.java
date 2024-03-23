135a2c7b5052ac1510509dcd830e46f38acbbde7
sriram <sriramoholic254@gmail.com>
2024-03-23 20:10:57 +0530

    Modified, Staticblock.java

--- StaticBlock.java
+++ StaticBlock.java
@@ -1,18 +1,13 @@
 class Mobile{
-    String brand;
-    int price;
-    static String name;
 
     static{
-        name = "SmartPhone";
         System.out.println("Inside Static Block");
         //The static block will get initialised only once irrespective of how many objects are created. The static block will be called only once.
     }
 
-    public Mobile(){ //Constructor -> A constructor in Java is a special method that is used to initialize objects. The constructor is called when an object of a class is created. It can be used to set initial values for object attributes
-        brand = "Samsung";
-        price = 1000;
-        System.out.println("In Constructor");
+    public Mobile(){ 
+        //Constructor -> A constructor in Java is a special method that is used to initialize objects. The constructor is called when an object of a class is created. It can be used to set initial values for object attributes
+        System.out.println("Inside the Constructor");
         //The constructor will get initialised each and every time an object is created.
     }
 }
@@ -20,12 +15,9 @@ class Mobile{
 public class StaticBlock {
     public static void main(String a[]) throws ClassNotFoundException{
         Class.forName("Mobile");
-        //Class will loaded only once and only when the object is created. But, if we need to load class without creating an object, This is the way. (Class.forName())
+        //Class will get loaded only once and only when the object is created. But, if we need to load class without creating an object, This is the way. (Class.forName())
 
-        // Mobile obj1 = new Mobile();
-        // System.out.println("Inside Object 1");
-
-        // Mobile obj2 = new Mobile();
-        // System.out.println("Inside object 2");
+        Mobile obj1 = new Mobile();
+        Mobile obj2 = new Mobile();
     }
 }
