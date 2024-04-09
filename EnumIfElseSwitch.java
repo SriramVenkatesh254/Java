8bd6cbd1b1bf40fe6575d7e12c68142ff4373141
sriram <sriramoholic254@gmail.com>
2024-04-09 23:45:12 +0530

    Added EnumIfElseSwitch

--- EnumIfElseSwitch.java
+++ EnumIfElseSwitch.java
@@ -1,5 +1,37 @@
+enum Day{
+    Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+};
+
 public class EnumIfElseSwitch {
     public static void main(String args[]){
-        
+
+        //ENUM IF AND ELSE
+        // Day day = Day.Wednesday;
+        // if(day == Day.Monday){
+        //     System.out.println("Monday");
+        // }
+        // else if(day == Day.Tuesday){
+        //     System.out.println("Tuesday");
+        // }
+        // //Remaining code goes.
+        // else{
+        //     System.out.println("Sunday");
+        // }
+
+        Day day = Day.Monday;
+        switch (day) {
+            case Monday:
+                System.out.println("Monday");
+                break;
+            case Tuesday:
+                System.out.println("Tuesday");
+                break;
+            case Wednesday:
+                System.out.println("Wednesday");
+                break;
+            default:
+                System.out.println("Sunday");
+                break;
+        }
     }
 }
