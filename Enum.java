3e0290c10282a784b34481c8c5620464622f645f
sriram <sriramoholic254@gmail.com>
2024-04-09 23:16:22 +0530

    Added Interface.java and Enum.java

---
+++ Enum.java
@@ -0,0 +1,18 @@
+// In Java, an enum (short for "enumeration") is a special data type used to define a set of named constants. Enums provide a way to represent a fixed set of predefined values, making the code more readable, maintainable, and type-safe.
+
+enum Status{
+    Running, Pending, Failed, Success
+};
+
+
+public class Enum {
+    public static void main(String args[]){
+        Status s = Status.Failed;
+        // System.out.println(s.ordinal()); //Returns the Index value
+        Status[] ss = Status.values(); //Returns the Entire array of the Enum
+        for(Status r : ss){
+            System.out.println(r);
+        }
+
+    }
+}
---
+++ Interface.java
@@ -0,0 +1,64 @@
+//Using CLASS AND OBJECTS.
+
+// abstract class Computer{
+//     public abstract void code();
+// }
+
+// class Laptop extends Computer{
+//     public void code(){
+//         System.out.println("Code, Compile, Run...");
+//     }
+// }
+
+// class Desktop extends Computer{
+//     public void code(){
+//         System.out.println("Code, Compile, Run : Faster");
+//     }
+// }
+
+// class Developer{
+//     public void devApp(Computer computer){
+//         computer.code();
+//     }
+// }
+
+// public class Interface {
+//     public static void main(String args[]){
+//         Computer lap = new Laptop();
+//         Computer desk = new Desktop();
+//         Developer sriram = new Developer();
+//         sriram.devApp(lap);
+//     }
+// }
+
+//Using INTERFACES.
+interface Computer{
+    void code();
+}
+
+class Laptop implements Computer{
+    public void code(){
+        System.out.println("Code, Compile, Run...");
+    }
+}
+
+class Desktop implements Computer{
+    public void code(){
+        System.out.println("Code, Compile, Run : Faster");
+    }
+}
+
+class Developer{
+    public void devApp(Computer computer){
+        computer.code();
+    }
+}
+
+public class Interface {
+    public static void main(String args[]){
+        Computer lap = new Laptop();
+        Computer desk = new Desktop();
+        Developer sriram = new Developer();
+        sriram.devApp(lap);
+    }
+}
