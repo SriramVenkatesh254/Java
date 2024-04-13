ca610601b33c103baae7d1e46093e5bec7181b01
sriram <sriramoholic254@gmail.com>
2024-04-13 21:20:32 +0530

    Renamed from Demo22 to FunctionalInterface.java

--- FunctionalInterface.java
+++ FunctionalInterface.java
@@ -13,7 +13,7 @@ interface A
 //     }
 // }
 
-public class Demo22 {
+public class FunctionalInterface {
     public static void main(String args[]){
         A obj = new A(){
             public void show(){
