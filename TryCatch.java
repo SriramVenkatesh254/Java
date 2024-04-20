ad421afd20f32f1c75ed83442cf424de6ebbb95c
sriram <sriramoholic254@gmail.com>
2024-04-20 17:52:59 +0530

    Modified TryCatch.java

--- TryCatch.java
+++ TryCatch.java
@@ -1,23 +1,17 @@
 public class TryCatch {
     public static void main(String args[]){
-        // int i = 0;
-        // int j = 0;
+        int i = 0;
+        int j = 0;
 
         try{
-            // j = 9/i;
-            int x[] = new int[5];
-            x[6] = 15;
+            j = 18/i;
+            // int x[] = new int[5];
+            // x[6] = 15;
 
             
         }
-        catch(ArithmeticException e){
-            System.out.println("OOPS!, Something went wrong " + e);
-        }
-        catch(ArrayIndexOutOfBoundsException e){
-            System.out.println("Too much length");
-        }
         catch(Exception e){
-            System.out.println(e);
+            System.out.println("OOPS!, Something went wrong " + e);
         }
         System.out.println(j);
         System.out.println("Bye");
